fb6f2c20b5bc3d38a4898688e1acb20f21776451
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-12 15:28:36 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 import {Country} from "./Country";
 import {v1} from "uuid";
 
-export type BanknotsType = '' // создадим типы для banknotes -он может быть 'DOLLARS', 'RUBLS' или 'All'
+export type BanknotsType = 'USD' | 'RUB' | 'All' // создадим типы для banknotes -он может быть 'DOLLARS', 'RUBLS' или 'All'
 export type MoneyType = {
     banknote: BanknotsType
-    nominal: any// не ленимся, убираем заглушку, и пишем правильный тип)
-    id: any// ложку за Димыча, за...
+    nominal: number // не ленимся, убираем заглушку, и пишем правильный тип)
+    id: string // ложку за Димыча, за...
 }
 
-let defaultMoney: any = [  // типизируем
+let defaultMoney: MoneyType[] = [  // типизируем
     {banknote: 'USD', nominal: 100, id: v1()},
     {banknote: 'USD', nominal: 100, id: v1()},
     {banknote: 'RUB', nominal: 100, id: v1()},
@@ -22,7 +22,11 @@ let defaultMoney: any = [  // типизируем
 ]
 
 
-export const moneyFilter = (money: any, filter: any): any => {
+export const moneyFilter = (money: MoneyType[], filter: BanknotsType): MoneyType[] => {
+    if (filter === 'All') {
+        return money
+    }
+    return money.filter(el => el.banknote === filter)
     //если пришел filter со значением 'All', то возвращаем все банкноты
     //return money.filter... ну да, придется фильтровать
 }
@@ -30,12 +34,12 @@ export const moneyFilter = (money: any, filter: any): any => {
 
 function App() {
     // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
-    const [money, setMoney] = useState<any>([])
-    const [filterValue, setFilterValue] = useState<any>('')   // по умолчанию указываем все банкноты
+    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
+    const [filterValue, setFilterValue] = useState<BanknotsType>('All')   // по умолчанию указываем все банкноты
 
     // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
     // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-    const filteredMoney = moneyFilter(грошы, фильтръ)
+    const filteredMoney = moneyFilter(money , filterValue) //(грошы, фильтръ)
 
     const addMoney = (banknote: BanknotsType) => {
         // Добавление денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
